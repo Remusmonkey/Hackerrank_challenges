a1db96390de5ed4d283aa01e735d35c4e3f830d0
Remusmonkey <127868049+Remusmonkey@users.noreply.github.com>
2024-11-16 18:26:07 -0800

    commiting but was having problems

--- minion_game.py
+++ minion_game.py
@@ -1,17 +1,21 @@
 def minion_game(string):
-    player1 = "Kevin"
-    player2 = "Stuart"
     vowels = ["A","E","I","O","U"]
     v_count = 0
     c_count = 0
-    for i in range(0,len(string)):
-        sub_str= string[i:]
-        
-        if string[i] in vowels:
-            v_count += 1
+    up_string = string.upper()
+
+    for i in range(0,len(up_string)):
+
+        if up_string[i] in vowels:
+            v_count += len(string) - i
         else:
-            c_count += 1
-    print(v_count, c_count)
+            c_count += len(string) - i
+    if v_count > c_count:
+        print(f"Kevin {v_count}")
+    elif v_count < c_count:
+        print(f"Stuart {c_count}")
+    else:
+        print("Draw")
 
 
 minion_game("Banana")
