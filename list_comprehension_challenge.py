683541ab363f2f50ab269898904b3e54e48b115a
Remusmonkey <127868049+Remusmonkey@users.noreply.github.com>
2024-12-01 16:19:53 -0800

    Completed Runner up challenge and did some more list comprehension practice

--- list_comprehension_challenge.py
+++ list_comprehension_challenge.py
@@ -12,7 +12,38 @@
 # 
 # comp = [(i, j, k) for i in range(x + 1) for j in range(y + 1) for k in range(z + 1)]
 # final = [for
-x, y, z = 1, 1, 1  # Example dimensions
-n=2
-result = [(i, j, k) for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if i + j + k != n]
-print(result)
+# x, y, z = 1, 1, 1  # Example dimensions
+# n=2
+# result = [(i, j, k) for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if i + j + k != n]
+# print(result)
+
+# 1. chatgpt
+# sq_list = [1, 2, 3, 4, 5]
+# result = [x**2 for x in sq_list]
+# print(result)
+
+# 2. Chatgpt
+# to_filter = [10, 15, 20, 25, 30]
+# result= [x for x in to_filter if x % 2 == 0]
+# print(result)
+
+# 3. Chatgpt
+# n = 3
+# result = [n*i for i in range(1,11)]
+# print(result)
+
+# 4. Chatgpt Flaten nested list
+# a = [[1, 2, 3], [4, 5], [6, 7, 8, 9]]
+# result= [ c for i in a for c in i]
+# print(result)
+
+# 5. chatgpt Remove vowels
+# a = "This is a test sentence"
+# v = (A,a,E,e,I,i,O,o,U,u)
+# result=[i for i in a if i not in "aeiouAEIOU"]
+# print("".join(result))
+
+
+
+
+         
---
+++ runner up score challenge.py
@@ -0,0 +1,29 @@
+n = 5
+arr = [2,3,6,6,5]
+n1 =4
+arr1= [-10,0,10]
+top_score= max(arr1)
+
+max_removed = [x for x in arr1 if x!= top_score]
+print(max(max_removed))
+print(max_removed)
+
+
+
+
+
+
+
+
+#index=0
+# while index < len(arr):
+#     element = arr[index]
+#     print(element)
+#     index += 1
+    
+#result = [i for i in arr if i not in i]
+# result= {i for i in arr}
+# result2 = [i for i in result]
+# print(result2[-2])
+# print(result2)
+# print(max(arr))
